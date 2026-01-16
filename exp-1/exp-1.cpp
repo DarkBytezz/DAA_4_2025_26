8480789fb2a74cd52514832b29564534ee4c489d
DarkBytezz <darkkbytezz@gmail.com>
2026-01-16 19:41:52 +0530

    Second push

---
+++ exp-1/exp-1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int currentDepth = 0;
+int maxDepth = 0;
+
+void complexRec(int n){
+    currentDepth++;
+    maxDepth = max(maxDepth, currentDepth);
+
+    if (n <= 2){
+        currentDepth--;
+        return;
+    }
+
+    int p = n;
+    while (p > 0){
+        vector<int>temp(n);
+        for (int i = 0; i < n; i++){
+            temp[i] = i ^ p;
+        }
+        p >>= 1;
+    }
+
+    vector<int>small(n);
+    for (int i = 0; i < n; i++){
+        small[i] = i * i;
+    }
+
+    reverse(small.begin(), small.end());
+
+    complexRec(n / 2);
+    complexRec(n / 2);
+    complexRec(n / 2);
+
+    currentDepth--;
+}
+
+int main()
+{
+    int n;cin>>n;
+    complexRec(n);
+    cout << "Max recursion depth: " << maxDepth << endl;
+    return 0;
+}
+
+// Recurance relation: 3T(n/2) + (n * log n)
+// Time complexity: O(n^log_base2^3)
